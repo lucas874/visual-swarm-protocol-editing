8f965726a09efca6438c4593da998b2d60b0a745
Caroline Fallesen <carolinefallesen@gmail.com>
2024-11-23 17:56:18 +0100

    Remove unused test file and CSS, simplify ReactDOM rendering

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ReactFlowProvider } from "reactflow";
-import "./index.css";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(
-    <ReactFlowProvider>
-      <App />
-    </ReactFlowProvider>
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
