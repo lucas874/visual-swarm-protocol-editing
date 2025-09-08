79649530afb4f00783e1c2cc0985b96034229223
lucas874 <lsc500@outlook.dk>
2025-09-08 15:28:06 +0200

    Trying things with ts-morph

--- src/parse-protocols.ts
+++ src/parse-protocols.ts
@@ -1,4 +1,4 @@
-import { Project, Node, SyntaxKind, VariableDeclaration, CallExpression, TypeAliasDeclaration, ObjectLiteralExpression, PropertyAssignment, Identifier } from "ts-morph";
+import { Project, Node, SyntaxKind, VariableDeclaration, CallExpression, TypeAliasDeclaration, ObjectLiteralExpression, PropertyAssignment, Identifier, SourceFile, StringLiteral, ts } from "ts-morph";
 import { SwarmProtocolType } from "@actyx/machine-check";
 type Occurrence = { name: string, jsonObject: SwarmProtocolType}
 
@@ -29,108 +29,116 @@ export const getValue = <T>(optionValue: Some<T>): T => {
 export function parseProtocols(fileName: string): any[] {
   const project = new Project({tsConfigFilePath: "/home/luccl/Git/visual-swarm-protocol-editing/tsconfig.json"});
   const sourceFile = project.addSourceFileAtPath(fileName);
-  const visitor = new CollectingVisitor();
-  traverse(sourceFile, visitor)
-    throw Error
+  visitVariableDeclarations(sourceFile)
+  throw Error("Not implemented")
 }
 
-// Visitor interface
-interface ASTVisitor {
-  visitVariableDeclaration?(node: VariableDeclaration): void;
-  visitTypeAliasDeclaration?(node: TypeAliasDeclaration): void;
-  // Fallback for unhandled nodes
-  visitNode?(node: Node): void;
-}
-
-// Traversal function — calls specialized visit methods if available
-function traverse(node: Node, visitor: ASTVisitor) {
-  switch (node.getKind()) {
-    case SyntaxKind.VariableDeclaration:
-      visitor.visitVariableDeclaration?.(node as VariableDeclaration);
-      break;
-    case SyntaxKind.TypeAliasDeclaration:
-      visitor.visitTypeAliasDeclaration?.(node as TypeAliasDeclaration);
-      break;
-    default:
-      visitor.visitNode?.(node);
-  }
-
-  node.forEachChild(child => traverse(child, visitor));
-}
-
-// Traverse an ast constructing an collecting instances of 'SwarmProtocolType'
-class CollectingVisitor implements ASTVisitor {
-  protocols: Occurrence[] = [];
-
-  childWithKind(node: Node, kind: SyntaxKind): boolean {
-    return node.getChildrenOfKind(kind).length > 0;
-  }
-
-  // Find child that is a TypeNode or undefined
-  childTypeNode(node: Node): Node | undefined {
-    return node.getChildren().find(child => Node.isTyped(child));
-  }
-
-  // Events are defined using MachineEvent.design('event type')...
-  // Extract 'event type'
-  getEventTypeNameFromArgs(node: CallExpression): string {
-    const args = node.getArguments();
-    //basicVisit(node)
-    if (args && args.length > 0) {
-      if (args[0]!.getKind() === SyntaxKind.StringLiteral) {
-        return args[0]!.getText().replace(/['"]/g, '');
-      } else if (args[0]?.getKind() === SyntaxKind.Identifier) {
-      }
-    }
-    throw new Error(`Event type name not found in arguments of call expression: ${node.getText()}`);
-  }
-
-  swarmProtocolDeclaration(node: VariableDeclaration): Option<Occurrence> {
+function swarmProtocolDeclaration(node: VariableDeclaration): Option<Occurrence> {
     switch (node.getInitializer().getKind()) {
         case SyntaxKind.ObjectLiteralExpression:
-            const properties = (node.getInitializer() as ObjectLiteralExpression).getProperties().map(e => (e as PropertyAssignment))
+            const properties = new Map((node.getInitializer() as ObjectLiteralExpression)
+                .getProperties()
+                .map(e => (e as PropertyAssignment))
+                .map(p => [p.getName(), p]))
 
-            for (const p of properties) {
-                //console.log("keys: ", Object.keys(p))
-                //console.log(p)
-
-                //console.log(Object.getPrototypeOf(p))
-                console.log(p.getName())
-                //console.log(p.getChildren().map(e => e.getKindName()))
-                basicVisit(p)
-                console.log("----")
+            const initial = properties.get("initial")
+            if (initial) {
+                const expandedInitializer = getInitializerInitial(initial)
+                if (isSome(expandedInitializer)) {
+                    console.log("hej hej okkkk: ", expandedInitializer)
+                    properties.set("initial", getValue(expandedInitializer))
+                }
             }
-
-
+            const transitions = properties.get("transitions")
+            if (transitions) {
+                const expandedInitializer = getInitializerTransitions(transitions)
+            }
         }
 
     return none
 
   }
 
-  visitVariableDeclaration(node: VariableDeclaration) {
-    console.log("Variable declaration")
-
-    console.log("Initializer: ", node.getInitializer())
-    console.log("Initializer: ", node.getInitializer().getKindName())
-    if (this.swarmProtocolDeclaration(node)) {
-        basicVisit(node)
+function literalInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
+    basicVisit(node)
+    switch (node.getKind()) {
+        case SyntaxKind.StringLiteral:
+        case SyntaxKind.NumericLiteral:
+        case SyntaxKind.BigIntLiteral:
+        case SyntaxKind.TrueKeyword:
+        case SyntaxKind.FalseKeyword:
+            return some(node)
+        case SyntaxKind.Identifier:
+            console.log(node)
+            const definitionNodes = (node as Identifier).getDefinitionNodes()
+            for (const node of definitionNodes) {
+                console.log(node)
+                console.log("Def in: ", node.getSourceFile().getFilePath())
+            }
+            if (definitionNodes.length > 0) {
+                return literalInitializer(definitionNodes[0]) // Assume there is just one definition of this name
+            } else {
+                return none
+            }
+        case SyntaxKind.VariableDeclaration:
+            console.log("encountered a variable declaration: ")
+            console.log("initializer is: ", (node as VariableDeclaration).getInitializer())
+            return literalInitializer((node as VariableDeclaration).getInitializer())
     }
 
-  }
-
-  // Insert a type variable into eventSpec.typeVariables
-  visitTypeAliasDeclaration(node: TypeAliasDeclaration) {
-    console.log("Type alias declaration")
-    const typeNode = node.getTypeNode();
-    //basicVisit(node)
-  }
+    return none
 }
 
+function getInitializerInitial(node: PropertyAssignment): Option<PropertyAssignment> {
+    const initializer = node.getInitializer()
+    console.log("initializer: ", initializer)
+    const literalOption = literalInitializer(initializer)
+    if (isSome(literalOption)) {
+        const literal = getValue(literalOption)
+        console.log(literal)
+        console.log(literal.getText())
+        if (literal.getKind() === SyntaxKind.StringLiteral) {
+            return some(node.setInitializer(literal.getText()))
+        }
+    }
+    /* switch (initializer.getKind()) {
+        case SyntaxKind.StringLiteral:
+            return some(node)
+        case SyntaxKind.Identifier:
+            console.log(node)
+            const definitionNodes = (initializer as Identifier).getDefinitionNodes()
+            for (const node of definitionNodes) {
+                console.log(node)
+                console.log("Def in: ", node.getSourceFile().getFilePath())
+                basicVisit(node)
 
+            }
+
+
+            return none
+    }
+
+    return none */
+
+}
+
+function visitVariableDeclarations(sourceFile: SourceFile) {
+    const variableDeclarations = sourceFile.getVariableDeclarations()
+    const swarmProtocols = variableDeclarations.map(variableDeclaration => swarmProtocolDeclaration(variableDeclaration)).filter(o => isSome(o))
+    return swarmProtocols
+
+}
 
 // Nice for debugging
 function basicVisit(node: Node, prepend: string = '') {
+  console.log(`${prepend}Node: ${node.getText()} of kind ${SyntaxKind[node.getKind()]}`);
+  node.forEachChild(child => {
+    basicVisit(child, prepend + '  * ');
+  });
+}
+
+// Nice for debugging
+/* function basicVisit(node: Node, prepend: string = '') {
   console.log(`${prepend}Node: ${node.getText()} of kind ${SyntaxKind[node.getKind()]}`);
   if (node.getKind() === SyntaxKind.Identifier) {
     const a = (node as Identifier).getDefinitions()
@@ -141,7 +149,7 @@ function basicVisit(node: Node, prepend: string = '') {
         console.log(d)
         console.log("Def in: ", d.getSourceFile().getFilePath())
     }
-    /* const symbol = node.getSymbolOrThrow()
+    const symbol = node.getSymbolOrThrow()
     console.log("symbol: ", symbol)
     const declarations = symbol.getDeclarations();
 
@@ -149,9 +157,9 @@ function basicVisit(node: Node, prepend: string = '') {
         const declSourceFile = decl.getSourceFile();
         console.log("Defined in:", declSourceFile.getFilePath());
         console.log("Declaration text:", decl.getText());
-    } */
+    }
   }
   node.forEachChild(child => {
     basicVisit(child, prepend + '  * ');
   });
-}
+} */
