ad4c39ce6c8ec0f0158d6401ae4d59711395fdf8
lucas874 <lsc500@outlook.dk>
2025-09-10 10:12:21 +0200

    Remove prints

--- src/parse-protocols.ts
+++ src/parse-protocols.ts
@@ -72,10 +72,6 @@ function literalInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
             return some(node)
         case SyntaxKind.Identifier:
             const definitionNodes = (node as Identifier).getDefinitionNodes()
-            for (const node of definitionNodes) {
-                console.log(node)
-                console.log("Def in: ", node.getSourceFile().getFilePath())
-            }
             if (definitionNodes.length > 0) {
                 return literalInitializer(definitionNodes[0]) // Assume there is just one definition of this name
             } else {
@@ -86,8 +82,6 @@ function literalInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
         case SyntaxKind.ObjectLiteralExpression:
             return objectLiteralInitializer(node as ObjectLiteralExpression)
         case SyntaxKind.PropertyAccessExpression:
-            console.log((node as PropertyAccessExpression).getName())
-            console.log((node as PropertyAccessExpression).getNameNode())
             return literalInitializer((node as PropertyAccessExpression).getNameNode())
         case SyntaxKind.VariableDeclaration:
             return literalInitializer((node as VariableDeclaration).getInitializer())
@@ -98,7 +92,6 @@ function literalInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
 
 // Give every array item a 'literal' initializer.
 function arrayLiteralInitializer(node: ArrayLiteralExpression, getInitializer: ((e: Expression<ts.Expression>) => Option<Node<ts.Node>>)): Option<Node<ts.Node>> {
-    console.log("array: ", node.getText());
     node.getElements().forEach(e => {
         const literal = getInitializer(e)
         if (isSome(literal)) {
@@ -108,7 +101,6 @@ function arrayLiteralInitializer(node: ArrayLiteralExpression, getInitializer: (
         }
     })
 
-    console.log("array after: ", node.getText());
     return some(node)
 }
 
@@ -116,7 +108,6 @@ function objectLiteralInitializer(node: ObjectLiteralExpression): Option<Node<ts
     // Transform properties to literal.
     // Remove the properties
     // Add the literal properties
-    console.log("object: ", node.getText());
     //basicVisit(node)
     const properties = (node as ObjectLiteralExpression)
         .getProperties()
@@ -127,7 +118,6 @@ function objectLiteralInitializer(node: ObjectLiteralExpression): Option<Node<ts
                 initializer: property.getName() === "logType" ? arrayLiteralInitializer(property.getInitializer() as ArrayLiteralExpression, handleLogTypeInitializer) : literalInitializer(property.getInitializer())
             }
         });
-    console.log("properties: ", properties);
     node.getProperties().forEach(p => p.remove())
     for (const property of properties) {
         if (isSome(property.initializer)) {
@@ -137,18 +127,14 @@ function objectLiteralInitializer(node: ObjectLiteralExpression): Option<Node<ts
         }
 
     }
-    console.log("object after: ", node.getText());
     return some(node)
 }
 
 function getInitializerInitial(node: PropertyAssignment): Option<PropertyAssignment> {
     const initializer = node.getInitializer()
-    console.log("initializer: ", initializer)
     const literalOption = literalInitializer(initializer)
     if (isSome(literalOption)) {
         const literal = getValue(literalOption)
-        console.log(literal)
-        console.log(literal.getText())
         if (literal.getKind() === SyntaxKind.StringLiteral) {
             return some(node.setInitializer(literal.getText()))
         }
@@ -158,13 +144,9 @@ function getInitializerInitial(node: PropertyAssignment): Option<PropertyAssignm
 
 function getInitializerTransitions(node: PropertyAssignment): Option<PropertyAssignment> {
     const initializer = node.getInitializer()
-    console.log("initializer: ", initializer)
     const literalOption = literalInitializer(initializer)
     if (isSome(literalOption)) {
         const literal = getValue(literalOption)
-        console.log("literal: ", literal)
-        console.log("literal text: ", literal.getText())
-
     }
     return none
 }
@@ -188,29 +170,17 @@ function getInitializerTransitions(node: PropertyAssignment): Option<PropertyAss
  * @returns Option<Node<ts.Node>>
  */
 function handleLogTypeInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
-    console.log("hello in handleLogTypeInitializer: ", node)
     switch (node.getKind()) {
         case SyntaxKind.StringLiteral:
-            console.log("found string literal: ", node)
-            console.log("string literal text: ", node.getText())
             return some(node)
         case SyntaxKind.Identifier:
-            console.log("Found identifier")
             const definitionNodes = (node as Identifier).getDefinitionNodes()
-            for (const node of definitionNodes) {
-                console.log(node)
-                console.log("Def in: ", node.getSourceFile().getFilePath())
-            }
             if (definitionNodes.length > 0) {
-                console.log("boing identifier")
-                console.log("identifier definitionNodes[0]: ", definitionNodes[0])
-                //return none
                 return handleLogTypeInitializer(definitionNodes[0]) // Assume there is just one definition of this name
             } else {
                 return none
             }
         case SyntaxKind.PropertyAccessExpression:
-            console.log("Hi in PropertyAccessExpression")
             const definitionNodesProperty = (node as PropertyAccessExpression).getNameNode().getDefinitionNodes()
             // Assume one definition
             if (definitionNodesProperty.length > 0) {
@@ -218,7 +188,6 @@ function handleLogTypeInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
                 if (definitionNode.getSourceFile().getFilePath().endsWith("event.d.ts")
                     && definitionNode.getKind() == SyntaxKind.PropertySignature
                     && (definitionNode as PropertySignature).getName() === "type") {
-                    console.log("ok conditional")
                     handleLogTypeInitializer((node as PropertyAccessExpression).getExpression())
 
                 } else {
@@ -226,7 +195,6 @@ function handleLogTypeInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
                 }
             }
         case SyntaxKind.VariableDeclaration:
-            console.log("In variable declaration: (node as VariableDeclaration).getInitializer(): ", (node as VariableDeclaration).getInitializer())
             return handleLogTypeInitializer((node as VariableDeclaration).getInitializer())
         case SyntaxKind.CallExpression:
                 // Check if this is a call to withPayload or withoutPayload. If so get parent propertyAccessExpression and go back to call to design.
@@ -235,7 +203,7 @@ function handleLogTypeInitializer(node: Node<ts.Node>): Option<Node<ts.Node>> {
              console.log("In call expression: (node as CallExpression).getText(): ", (node as CallExpression).getText())
              console.log("In call expression: (node as CallExpression): ", (node as CallExpression))
              console.log((node as CallExpression).getArguments())
-            console.log((node as CallExpression).getExpression().getText())
+            console.log((node as CallExpression).getExpression().getLastToken().getText())
 
             console.log((node as CallExpression).getExpression().getKindName())
         default:
@@ -258,30 +226,3 @@ function basicVisit(node: Node, prepend: string = '') {
         basicVisit(child, prepend + '  * ');
     });
 }
-
-// Nice for debugging
-/* function basicVisit(node: Node, prepend: string = '') {
-  console.log(`${prepend}Node: ${node.getText()} of kind ${SyntaxKind[node.getKind()]}`);
-  if (node.getKind() === SyntaxKind.Identifier) {
-    const a = (node as Identifier).getDefinitions()
-    console.log("a: ", a)
-    const b = (node as Identifier).getDefinitionNodes()
-    console.log("b: ", b)
-    for (const d of b) {
-        console.log(d)
-        console.log("Def in: ", d.getSourceFile().getFilePath())
-    }
-    const symbol = node.getSymbolOrThrow()
-    console.log("symbol: ", symbol)
-    const declarations = symbol.getDeclarations();
-
-    for (const decl of declarations) {
-        const declSourceFile = decl.getSourceFile();
-        console.log("Defined in:", declSourceFile.getFilePath());
-        console.log("Declaration text:", decl.getText());
-    }
-  }
-  node.forEachChild(child => {
-    basicVisit(child, prepend + '  * ');
-  });
-} */
