559bb97d5672c58444e00360ed2d19a1967f171a
lucas874 <lsc500@outlook.dk>
2025-09-15 16:07:01 +0200

    Minor

--- test-files/warehouse_protocol_1.ts
+++ test-files/warehouse_protocol_1.ts
@@ -5,7 +5,7 @@ type PartPayload = {partName: string}
 export const part = MachineEvent.design('part').withPayload<PartPayload>()
 
 // Thes same has warehouse_protocol, but uses the string literal 'time' instead of referring to some defined event
-export const warehouse: SwarmProtocolType = {
+export const warehouse = {
   initial: initialState,
   transitions: [
     {source: '0', target: '1', label: {cmd: 'request', role: 'T', logType: [Events.partID.type]}},
